a7b432f84b23d30974e6e392bfa17bc3c5af6942
Nud <nudemeth@hotmail.com>
2017-11-19 14:57:50 +0700

    Refactor NashornEngine

--- src/main/scala/com/nudemeth/example/web/engine/NashornEngine.scala
+++ src/main/scala/com/nudemeth/example/web/engine/NashornEngine.scala
@@ -5,40 +5,33 @@ import javax.script.CompiledScript
 import jdk.nashorn.api.scripting.{NashornScriptEngine, NashornScriptEngineFactory, ScriptObjectMirror}
 
 object NashornEngine {
+  private var instance: Option[NashornEngine] = None
+  def apply(scripts: Seq[ScriptSource]): NashornEngine = {
+    instance match {
+      case Some(_) =>
+      case None => instance = Some(new NashornEngine(scripts))
+    }
+    instance.get
+  }
+}
+
+sealed class NashornEngine private(scripts: Seq[ScriptSource]) extends JavaScriptEngine(scripts) {
   /*
-  Shared engine and compiled script. See links below:
-  https://stackoverflow.com/questions/30140103/should-i-use-a-separate-scriptengine-and-compiledscript-instances-per-each-threa
-  https://blogs.oracle.com/nashorn/nashorn-multithreading-and-mt-safety
+    Shared engine and compiled script. See links below:
+    https://stackoverflow.com/questions/30140103/should-i-use-a-separate-scriptengine-and-compiledscript-instances-per-each-threa
+    https://blogs.oracle.com/nashorn/nashorn-multithreading-and-mt-safety
   */
   private val engine = new NashornScriptEngineFactory()
     .getScriptEngine("-strict", "--no-java", "--no-syntax-extensions")
     .asInstanceOf[NashornScriptEngine]
-  private var compiledScript: Option[CompiledScript] = None
-}
 
-class NashornEngine(scripts: Seq[ScriptSource]) extends JavaScriptEngine(scripts) {
-  import NashornEngine._
-
-  private def init(): Unit = {
-    NashornEngine.compiledScript match {
-      case Some(_) =>
-      case None => NashornEngine.compiledScript = Some(compileScript())
-    }
-  }
-
-  private def appendScript(): String = {
-    scripts.map{
+  private val compiledScript: CompiledScript = {
+    val allScript = scripts.map{
       case ScriptText(s) => s
       case ScriptURL(s) => scala.io.Source.fromURL(s)("UTF-8").mkString
     }.mkString(sys.props("line.separator"))
+    engine.compile(allScript)
   }
-
-  private def compileScript(): CompiledScript = {
-    val allScript = appendScript()
-    NashornEngine.engine.compile(allScript)
-  }
-
-  init()
 
   /**
     * Invoking javascript object method. Always create new bindings when calling this because of multithreading environment
@@ -50,7 +43,7 @@ class NashornEngine(scripts: Seq[ScriptSource]) extends JavaScriptEngine(scripts
     */
   override def invokeMethod[T](objectName: String, methodName: String, args: Any*): T = {
     val bindings = engine.createBindings()
-    compiledScript.get.eval(bindings)
+    compiledScript.eval(bindings)
     val obj = bindings.get(objectName).asInstanceOf[ScriptObjectMirror]
     obj.callMember(methodName, args.map(_.asInstanceOf[AnyRef]): _*).asInstanceOf[T]
   }
