6ef49fd9c9785197fc98867db85c77ff2175e238
Nud <nudemeth@hotmail.com>
2017-12-20 21:04:32 +0700

    Add logger

--- src/main/scala/com/nudemeth/example/web/controller/BaseController.scala
+++ src/main/scala/com/nudemeth/example/web/controller/BaseController.scala
@@ -3,10 +3,12 @@ package com.nudemeth.example.web.controller
 import com.nudemeth.example.web.engine._
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.ScalatraServlet
+import org.slf4j.{Logger, LoggerFactory}
 
 trait BaseController extends ScalatraServlet {
 
   implicit val jsonFormats: Formats = DefaultFormats
+  protected val logger: Logger = LoggerFactory.getLogger(getClass)
 
   protected val renderer: JavaScriptEngine = J2V8Engine.instance.registerScripts(
     Seq(
